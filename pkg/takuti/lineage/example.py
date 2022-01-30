fe4d8d3fc35e90c62e33c1cf31528a5fb1cf8ce7
Takuya Kitazawa <k.takuti@gmail.com>
2022-01-30 13:07:25 -0800

    Mock webhook-based lineage backend

--- pkg/takuti/lineage/example.py
+++ pkg/takuti/lineage/example.py
@@ -1,8 +1,14 @@
 from airflow.lineage.backend import LineageBackend
-
+import requests
 
 class ExampleBackend(LineageBackend):
 
     def send_lineage(self, operator, inlets=None, outlets=None, context=None):
-        print('test')
-
+        url = "https://webhook.site/8fe779dc-10ae-4917-8f41-685723a31064"
+        data = {
+            "operator": operator,
+            "inlets": inlets,
+            "outlets": outlets,
+            "context": context,
+        }
+        requests.post(url, json=data)
