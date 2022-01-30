2dd99e254b8676cecd666d0a9634d350d82cc179
Takuya Kitazawa <k.takuti@gmail.com>
2022-01-30 13:21:25 -0800

    Convert operator to str

--- pkg/takuti/lineage/example.py
+++ pkg/takuti/lineage/example.py
@@ -6,7 +6,7 @@ class ExampleBackend(LineageBackend):
     def send_lineage(self, operator, inlets=None, outlets=None, context=None):
         url = "https://webhook.site/8fe779dc-10ae-4917-8f41-685723a31064"
         data = {
-            "operator": operator,
+            "operator": operator.__class__.__name__,
             "inlets": inlets,
             "outlets": outlets,
             "context": context,
